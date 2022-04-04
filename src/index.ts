e8d64877c19ecae556fcb139720256d1d204148b
lmachens <leon@machens.koeln>
2022-04-04 11:06:22 +0200

    ✨ Add fetch operations

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,103 @@
 import fetcher from './fetcher';
 
+export const postRegister = fetcher
+  .path('/auth/register')
+  .method('post')
+  .create();
+export const postAuthRefresh = fetcher
+  .path('/auth/refresh')
+  .method('post')
+  .create();
+export const postLogin = fetcher.path('/auth/login').method('post').create();
+
+export const getCurrentUser = fetcher.path('/user').method('get').create();
+export const putCurrentUser = fetcher.path('/user').method('put').create();
+export const patchCurrentUser = fetcher.path('/user').method('patch').create();
+export const getUser = fetcher.path('/user/{id}').method('get').create();
+
+export const getTutorials = fetcher.path('/tutorial').method('get').create();
+export const postTutorial = fetcher.path('/tutorial').method('post').create();
+export const getTutorial = fetcher
+  .path('/tutorial/{id}')
+  .method('get')
+  .create();
+export const putTutorial = fetcher
+  .path('/tutorial/{id}')
+  .method('put')
+  .create();
+
+export const getPurchasables = fetcher
+  .path('/purchasable')
+  .method('get')
+  .create();
+export const postPurchasable = fetcher
+  .path('/purchasable')
+  .method('post')
+  .create();
+export const getPurchasable = fetcher
+  .path('/purchasable/{id}')
+  .method('get')
+  .create();
+export const putPurchasable = fetcher
+  .path('/purchasable/{id}')
+  .method('put')
+  .create();
+export const deletePurchasable = fetcher
+  .path('/purchasable/{id}')
+  .method('delete')
+  .create();
+
 export const getGames = fetcher.path('/game').method('get').create();
+export const postGame = fetcher.path('/game').method('post').create();
+export const getGame = fetcher.path('/game/{id}').method('get').create();
+export const putGame = fetcher.path('/game/{id}').method('put').create();
+export const deleteGame = fetcher.path('/game/{id}').method('delete').create();
+export const putGameApiKey = fetcher
+  .path('/game/{id}/api-key')
+  .method('put')
+  .create();
+export const deleteGameApiKey = fetcher
+  .path('/game/{id}/api-key')
+  .method('delete')
+  .create();
+
+export const getMatches = fetcher.path('/match').method('get').create();
+export const getMatch = fetcher.path('/match/{id}').method('get').create();
+export const deleteMatch = fetcher
+  .path('/match/{id}')
+  .method('delete')
+  .create();
+export const postMatchJoin = fetcher
+  .path('/match/join')
+  .method('post')
+  .create();
+export const getReplay = fetcher
+  .path('/match/{matchId}/replay/{userId}')
+  .method('get')
+  .create();
+export const postReplay = fetcher
+  .path('/match/{matchId}/replay/{userId}')
+  .method('post')
+  .create();
+export const postScore = fetcher
+  .path('/match/{id}/score')
+  .method('post')
+  .create();
+
+export const getBasket = fetcher.path('/basket').method('get').create();
+export const deleteBasket = fetcher.path('/basket').method('delete').create();
+export const postBasketItems = fetcher
+  .path('/basket/items')
+  .method('post')
+  .create();
+export const patchBasketItems = fetcher
+  .path('/basket/items')
+  .method('patch')
+  .create();
+
+export const postCheckoutFast = fetcher
+  .path('/checkout/fast')
+  .method('post')
+  .create();
+
+export const getWallet = fetcher.path('/wallet').method('get').create();
--- test/fetch.test.ts
+++ test/fetch.test.ts
@@ -1,13 +1,448 @@
+import { ApiError } from 'openapi-typescript-fetch';
 import 'whatwg-fetch';
-import { getGames } from '../src/index';
+import {
+  deleteBasket,
+  deleteGame,
+  deleteGameApiKey,
+  deleteMatch,
+  deletePurchasable,
+  getBasket,
+  getCurrentUser,
+  getGame,
+  getGames,
+  getMatch,
+  getMatches,
+  getPurchasable,
+  getPurchasables,
+  getReplay,
+  getTutorial,
+  getTutorials,
+  getUser,
+  getWallet,
+  patchBasketItems,
+  patchCurrentUser,
+  postAuthRefresh,
+  postBasketItems,
+  postCheckoutFast,
+  postGame,
+  postLogin,
+  postMatchJoin,
+  postPurchasable,
+  postRegister,
+  postReplay,
+  postScore,
+  postTutorial,
+  putCurrentUser,
+  putGame,
+  putGameApiKey,
+  putPurchasable,
+  putTutorial,
+} from '../src/index';
 
+/**
+ * These tests should not replace backend tests, but make sure that the services are available.
+ */
 describe('fetch', () => {
-  it('GET /games', async () => {
-    const { ok, status, statusText, data } = await getGames({});
-
+  it('postRegister', async () => {
+    try {
+      await postRegister({
+        email: 'invalid_mail',
+        password: 'pw',
+      });
+      throw Error('Should fail without credentials');
+    } catch (error) {
+      const { status, statusText } = error as ApiError;
+      expect(status).toBe(409);
+      expect(statusText).toBe('Conflict');
+    }
+  });
+  it('postAuthRefresh', async () => {
+    try {
+      await postAuthRefresh({
+        refreshToken: 'fail!',
+      });
+      throw Error('Should fail without credentials');
+    } catch (error) {
+      const { status, statusText } = error as ApiError;
+      expect(status).toBe(409);
+      expect(statusText).toBe('Conflict');
+    }
+  });
+  it('postLogin', async () => {
+    try {
+      await postLogin({
+        email: 'invalid_mail',
+        password: 'pw',
+      });
+      throw Error('Should fail without credentials');
+    } catch (error) {
+      const { status, statusText } = error as ApiError;
+      expect(status).toBe(409);
+      expect(statusText).toBe('Conflict');
+    }
+  });
+  it('getCurrentUser', async () => {
+    try {
+      await getCurrentUser({});
+      throw Error('Should fail without credentials');
+    } catch (error) {
+      const { status, statusText } = error as ApiError;
+      expect(status).toBe(401);
+      expect(statusText).toBe('Unauthorized');
+    }
+  });
+  it('putCurrentUser', async () => {
+    try {
+      await putCurrentUser({
+        username: '',
+        firstname: '',
+        lastname: '',
+        avatarUrl: '',
+      });
+      throw Error('Should fail without credentials');
+    } catch (error) {
+      const { status, statusText } = error as ApiError;
+      expect(status).toBe(401);
+      expect(statusText).toBe('Unauthorized');
+    }
+  });
+  it('patchCurrentUser', async () => {
+    try {
+      await patchCurrentUser({
+        username: '',
+        avatarUrl: '',
+      });
+      throw Error('Should fail without credentials');
+    } catch (error) {
+      const { status, statusText } = error as ApiError;
+      expect(status).toBe(401);
+      expect(statusText).toBe('Unauthorized');
+    }
+  });
+  it('getUser', async () => {
+    try {
+      await getUser({ id: '1' });
+      throw Error('Should fail without credentials');
+    } catch (error) {
+      const { status, statusText } = error as ApiError;
+      expect(status).toBe(401);
+      expect(statusText).toBe('Unauthorized');
+    }
+  });
+  it('getTutorials', async () => {
+    try {
+      await getTutorials({ gameId: '1', pageable: {} });
+      throw Error('Should fail without credentials');
+    } catch (error) {
+      const { status, statusText } = error as ApiError;
+      expect(status).toBe(401);
+      expect(statusText).toBe('Unauthorized');
+    }
+  });
+  it('postTutorial', async () => {
+    try {
+      await postTutorial({ gameId: '1' });
+      throw Error('Should fail without credentials');
+    } catch (error) {
+      const { status, statusText } = error as ApiError;
+      expect(status).toBe(401);
+      expect(statusText).toBe('Unauthorized');
+    }
+  });
+  it('getTutorial', async () => {
+    try {
+      await getTutorial({ id: '1' });
+      throw Error('Should fail without credentials');
+    } catch (error) {
+      const { status, statusText } = error as ApiError;
+      expect(status).toBe(401);
+      expect(statusText).toBe('Unauthorized');
+    }
+  });
+  it('putTutorial', async () => {
+    try {
+      await putTutorial({ id: '1', finished: true });
+      throw Error('Should fail without credentials');
+    } catch (error) {
+      const { status, statusText } = error as ApiError;
+      expect(status).toBe(401);
+      expect(statusText).toBe('Unauthorized');
+    }
+  });
+  it('getPurchasables', async () => {
+    const { ok, status, statusText, data } = await getPurchasables({
+      pageable: {},
+    });
     expect(data.empty).toEqual(false);
     expect(ok).toBe(true);
     expect(status).toBe(200);
     expect(statusText).toBe('OK');
+  });
+  it('postPurchasable', async () => {
+    try {
+      await postPurchasable({
+        title: '',
+        description: '',
+        iconUrl: '',
+        bannerUrl: '',
+        type: '',
+        price: 1,
+        amount: 1,
+        properties: {},
+        tags: [],
+      });
+      throw Error('Should fail without credentials');
+    } catch (error) {
+      const { status, statusText } = error as ApiError;
+      expect(status).toBe(401);
+      expect(statusText).toBe('Unauthorized');
+    }
+  });
+  it('getPurchasable', async () => {
+    try {
+      await getPurchasable({
+        id: '-1',
+      });
+      throw Error('Should fail for not existing item');
+    } catch (error) {
+      const { status, statusText } = error as ApiError;
+      expect(status).toBe(404);
+      expect(statusText).toBe('Not Found');
+    }
+  });
+  it('putPurchasable', async () => {
+    try {
+      await putPurchasable({
+        id: '0',
+        title: '',
+        description: '',
+        iconUrl: '',
+        bannerUrl: '',
+        type: '',
+        price: 1,
+        amount: 1,
+        properties: {},
+        tags: [],
+      });
+      throw Error('Should fail without credentials');
+    } catch (error) {
+      const { status, statusText } = error as ApiError;
+      expect(status).toBe(401);
+      expect(statusText).toBe('Unauthorized');
+    }
+  });
+  it('deletePurchasable', async () => {
+    try {
+      await deletePurchasable({
+        id: '0',
+      });
+      throw Error('Should fail without credentials');
+    } catch (error) {
+      const { status, statusText } = error as ApiError;
+      expect(status).toBe(401);
+      expect(statusText).toBe('Unauthorized');
+    }
+  });
+  it('getGames', async () => {
+    const { ok, status, statusText, data } = await getGames({ pageable: {} });
+    expect(data.empty).toEqual(false);
+    expect(ok).toBe(true);
+    expect(status).toBe(200);
+    expect(statusText).toBe('OK');
+  });
+  it('postGame', async () => {
+    try {
+      await postGame({ title: '' });
+      throw Error('Should fail without credentials');
+    } catch (error) {
+      const { status, statusText } = error as ApiError;
+      expect(status).toBe(401);
+      expect(statusText).toBe('Unauthorized');
+    }
+  });
+  it('getGame', async () => {
+    try {
+      await getGame({
+        id: '-1',
+      });
+      throw Error('Should fail for not existing item');
+    } catch (error) {
+      const { status, statusText } = error as ApiError;
+      expect(status).toBe(404);
+      expect(statusText).toBe('Not Found');
+    }
+  });
+  it('putGame', async () => {
+    try {
+      await putGame({ id: '-1', title: '' });
+      throw Error('Should fail without credentials');
+    } catch (error) {
+      const { status, statusText } = error as ApiError;
+      expect(status).toBe(401);
+      expect(statusText).toBe('Unauthorized');
+    }
+  });
+  it('deleteGame', async () => {
+    try {
+      await deleteGame({ id: '-1' });
+      throw Error('Should fail without credentials');
+    } catch (error) {
+      const { status, statusText } = error as ApiError;
+      expect(status).toBe(401);
+      expect(statusText).toBe('Unauthorized');
+    }
+  });
+  it('putGameApiKey', async () => {
+    try {
+      await putGameApiKey({ id: '-1' });
+      throw Error('Should fail without credentials');
+    } catch (error) {
+      const { status, statusText } = error as ApiError;
+      expect(status).toBe(401);
+      expect(statusText).toBe('Unauthorized');
+    }
+  });
+  it('deleteGameApiKey', async () => {
+    try {
+      await deleteGameApiKey({ id: '-1' });
+      throw Error('Should fail without credentials');
+    } catch (error) {
+      const { status, statusText } = error as ApiError;
+      expect(status).toBe(401);
+      expect(statusText).toBe('Unauthorized');
+    }
+  });
+  it('getMatches', async () => {
+    try {
+      await getMatches({ playerToken: '' });
+      throw Error('Should fail without credentials');
+    } catch (error) {
+      const { status, statusText } = error as ApiError;
+      expect(status).toBe(401);
+      expect(statusText).toBe('Unauthorized');
+    }
+  });
+  it('getMatch', async () => {
+    try {
+      await getMatch({ id: '' });
+      throw Error('Should fail without credentials');
+    } catch (error) {
+      const { status, statusText } = error as ApiError;
+      expect(status).toBe(401);
+      expect(statusText).toBe('Unauthorized');
+    }
+  });
+  it('deleteMatch', async () => {
+    try {
+      await deleteMatch({ id: '' });
+      throw Error('Should fail without credentials');
+    } catch (error) {
+      const { status, statusText } = error as ApiError;
+      expect(status).toBe(401);
+      expect(statusText).toBe('Unauthorized');
+    }
+  });
+  it('postMatchJoin', async () => {
+    try {
+      await postMatchJoin({ playerToken: '' });
+      throw Error('Should fail without credentials');
+    } catch (error) {
+      const { status, statusText } = error as ApiError;
+      expect(status).toBe(401);
+      expect(statusText).toBe('Unauthorized');
+    }
+  });
+  it('getReplay', async () => {
+    try {
+      await getReplay({
+        matchId: '1',
+        userId: '1',
+      });
+      throw Error('Should fail without credentials');
+    } catch (error) {
+      const { status, statusText } = error as ApiError;
+      expect(status).toBe(401);
+      expect(statusText).toBe('Unauthorized');
+    }
+  });
+  it('postReplay', async () => {
+    try {
+      await postReplay({ matchId: '1', userId: '1', payload: 'a' });
+      throw Error('Should fail without credentials');
+    } catch (error) {
+      const { status, statusText } = error as ApiError;
+      expect(status).toBe(401);
+      expect(statusText).toBe('Unauthorized');
+    }
+  });
+  it('postScore', async () => {
+    try {
+      await postScore({ id: '1', playerToken: '1', score: 1 });
+      throw Error('Should fail without credentials');
+    } catch (error) {
+      const { status, statusText } = error as ApiError;
+      expect(status).toBe(401);
+      expect(statusText).toBe('Unauthorized');
+    }
+  });
+  it('getBasket', async () => {
+    try {
+      await getBasket({});
+      throw Error('Should fail without credentials');
+    } catch (error) {
+      const { status, statusText } = error as ApiError;
+      expect(status).toBe(401);
+      expect(statusText).toBe('Unauthorized');
+    }
+  });
+  it('deleteBasket', async () => {
+    try {
+      await deleteBasket({});
+      throw Error('Should fail without credentials');
+    } catch (error) {
+      const { status, statusText } = error as ApiError;
+      expect(status).toBe(401);
+      expect(statusText).toBe('Unauthorized');
+    }
+  });
+  it('postBasketItems', async () => {
+    try {
+      await postBasketItems({ purchasableId: '1', amount: 1 });
+      throw Error('Should fail without credentials');
+    } catch (error) {
+      const { status, statusText } = error as ApiError;
+      expect(status).toBe(401);
+      expect(statusText).toBe('Unauthorized');
+    }
+  });
+  it('patchBasketItems', async () => {
+    try {
+      await patchBasketItems({ purchasableId: '1', amount: 1 });
+      throw Error('Should fail without credentials');
+    } catch (error) {
+      const { status, statusText } = error as ApiError;
+      expect(status).toBe(401);
+      expect(statusText).toBe('Unauthorized');
+    }
+  });
+  it('postCheckoutFast', async () => {
+    try {
+      await postCheckoutFast({ purchasableId: '1' });
+      throw Error('Should fail without credentials');
+    } catch (error) {
+      const { status, statusText } = error as ApiError;
+      expect(status).toBe(401);
+      expect(statusText).toBe('Unauthorized');
+    }
+  });
+  it('getWallet', async () => {
+    try {
+      await getWallet({});
+      throw Error('Should fail without credentials');
+    } catch (error) {
+      const { status, statusText } = error as ApiError;
+      expect(status).toBe(401);
+      expect(statusText).toBe('Unauthorized');
+    }
   });
 });
