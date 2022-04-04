f61046e5a05cd810b58f3b91cade31b1f91f8e04
lmachens <leon@machens.koeln>
2022-04-04 09:20:29 +0200

    🔥 Remove example usage

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,3 @@
 import fetcher from './fetcher';
 
 export const getGames = fetcher.path('/game').method('get').create();
-getGames({}).then((a) => {
-  if (a.status === 200) {
-    a.data;
-  }
-});
