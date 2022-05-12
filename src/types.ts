e30c7bc9e3b38aff0f07be7a432de03d9cf56c93
lmachens <lmachens@users.noreply.github.com>
2022-05-12 00:34:15 +0000

    🏷️ Update types

--- src/types.ts
+++ src/types.ts
@@ -1260,7 +1260,7 @@ export interface operations {
           'application/json': components['schemas']['MatchResponse'];
         };
       };
-      /** Playertoken is valid but the match id is invalid */
+      /** API Key is not valid */
       400: {
         content: {
           'application/json': components['schemas']['ErrorResponse'];
